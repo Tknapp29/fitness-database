fa48e59a6e5d4310f5310466e3c4bdadae086c92
tknapp29 <tj29knap@siena.edu>
2024-12-02 23:19:42 -0500

    uploading sample data

--- SampleData.sql
+++ SampleData.sql
@@ -4,22 +4,34 @@
 
 //Trainers
 INSERT INTO Trainer (fname, age,gender, email,rating) VALUES
-('Rachel',40,'F', 'rgreen@letsgetfit.com', 5);
+('Alice',42,'F', 'aanderson@letsgetfit.com', 5);
 
 INSERT INTO Trainer (fname, age,gender, email,rating) VALUES
-('Ross',39,'M', 'rgeller@letsgetfit.com', 4);
+('Brain',39,'M', 'bbennett@letsgetfit.com', 4);
 
 INSERT INTO Trainer (fname, age,gender, email,rating) VALUES
-('Pheobe',40,'F', 'pbuffay@letsgetfit.com', 1);
+('Clara',40,'F', 'ccenter@letsgetfit.com', 1);
 
 INSERT INTO Trainer (fname, age,gender, email,rating) VALUES
-('Chandler',41,'M', 'cbing@letsgetfit.com', 5);
+('Daniel',41,'M', 'ddiaz@letsgetfit.com', 5);
 
 INSERT INTO Trainer (fname, age,gender, email,rating) VALUES
-('Joey',41,'M', 'jtribbiani@letsgetfit.com', 5);
+('Emma',41,'F', 'eevans@letsgetfit.com', 5);
 
 INSERT INTO Trainer (fname, age,gender, email,rating) VALUES
-('Monica',38,'M', 'mgeller@letsgetfit.com', 3);
+('Felix',38,'M', 'ffoster@letsgetfit.com', 1);
+
+INSERT INTO Trainer (fname, age,gender, email,rating) VALUES
+('Grace',30,'F', 'ggreen@letsgetfit.com', 4);
+
+INSERT INTO Trainer (fname, age,gender, email,rating) VALUES
+('Hannah',56,'F', 'hharris@letsgetfit.com', 5);
+
+INSERT INTO Trainer (fname, age,gender, email,rating) VALUES
+('Isaac',31,'M', 'iingram@letsgetfit.com', 2);
+
+INSERT INTO Trainer (fname, age,gender, email,rating) VALUES
+('Julia',21,'F', 'jjohnson@letsgetfit.com', 2);
 
 //trainer phone numbers
 INSERT into TrainerPhone VALUES
@@ -29,7 +41,7 @@ INSERT into TrainerPhone VALUES
 (1, '(123)-(654)-(9872)');
 
 INSERT into TrainerPhone VALUES
-(2, '(123)-(789)-(45655)');
+(2, '(123)-(789)-(4565)');
 
 INSERT into TrainerPhone VALUES
 (3, '(123)-(963)-(8528)');
@@ -44,26 +56,54 @@ INSERT into TrainerPhone VALUES
 (5, '(321)-(852)-(0025)');
 
 INSERT into TrainerPhone VALUES
-(6, '(123)-(456)-(2654)');
+(6, '(321)-(828)-(6969)');
+
+INSERT into TrainerPhone VALUES
+(7, '(321)-(420)-(5555)');
+
+INSERT into TrainerPhone VALUES
+(7, '(123)-(000)-(0001)');
+
+INSERT into TrainerPhone VALUES
+(8, '(321)-(888)-(7890)');
+
+INSERT into TrainerPhone VALUES
+(9, '(321)-(234)-(5678)');
+
+INSERT into TrainerPhone VALUES
+(10, '(321)-(999)-(4200)');
 
 //users
 INSERT INTO userTable VALUES
-('mscott', 'mscott@dundermifflin.com', 150, 'Micheal', 'M', 2);
+('kking', 'kking@email.com', 150, 'Kevin', 'M', 2);
 
 INSERT INTO userTable VALUES
-('dschrute', 'dschrute@dundermifflin.com', 180, 'Dwight', 'M', 4);
+('llewis', 'llewis@email.com', 180, 'Laura', 'F', 4);
 
 INSERT INTO userTable VALUES
-('jhalpert', 'jhalpert@dundermifflin.com', 160, 'Jim', 'M', 1);
+('mmiller', 'mmiller@email.com', 160, 'Micheal', 'M', 1);
 
 INSERT INTO userTable VALUES
-('shudson', 'shudson@dundermifflin.com', 180, 'Stantley', 'M', 1);
+('nnelson', 'nnelson@email.com', 180, 'Natalie', 'F', 2);
 
 INSERT INTO userTable VALUES
-('phalpert', 'phalpert@dundermifflin.com', 160, 'Pam' , 'F', 3);
+('oowens', 'oowens@email.com', 160, 'Oliver' , 'M', 3);
 
 INSERT INTO userTable VALUES
-('abernard' , 'abernard@dundermifflin.com' , 180, 'Andy' , 'M', 6);
+('pparker' , 'pparker@email.com' , 180, 'Penelope' , 'F', 6);
+
+INSERT INTO userTable VALUES
+('qquinn', 'qquinn@email.com', 160, 'Quentin', 'M', 7);
+
+INSERT INTO userTable VALUES
+('rroberts', 'rroberts@email.com', 180, 'Rachel', 'F', 8);
+
+INSERT INTO userTable VALUES
+('ssmith', 'ssmit@email.com', 160, 'Sam' , 'M', 9);
+
+INSERT INTO userTable VALUES
+('ttaylor' , 'ttaylor@email.com' , 180, 'Theresa' , 'F', 10);
+
 
 //exercises
 INSERT INTO exercise VALUES
@@ -80,6 +120,9 @@ INSERT INTO exercise VALUES
 
 INSERT INTO exercise VALUES
 ('Bench Press', 'Dumbell', 'Chest/Shoulders');
+
+INSERT INTO exercise VALUES
+('Bicep Curels', 'Chain Machine', 'Bicep/Back');
 
 INSERT INTO exercise VALUES
 ('Tricep Pulldown', 'Chain Machine', 'Chest/Shoulders');
@@ -107,66 +150,106 @@ INSERT INTO goal (goal, startDate, endDate) VALUES
 ('New Carloies record', DATE '17-11-24', DATE '2024-12-17');
 
 INSERT INTO goal (goal, startDate, endDate) VALUES 
-('Worked out 5 days in a row', DATE '17-11-24', DATE '2024-12-17');
+('Started a 3 day streak', DATE '17-11-24', DATE '2024-12-17');
 
 INSERT INTO goal (goal, startDate, endDate) VALUES 
-('Run a mile on the tred', DATE '17-11-24', DATE '2024-12-17');
+('First exercise', DATE '17-11-24', DATE '2024-12-17');
+
+INSERT INTO goal (goal, startDate, endDate) VALUES 
+('Worked out everyday this week', DATE '17-11-24', DATE '2024-12-17');
+
+INSERT INTO goal (goal, startDate, endDate) VALUES 
+('Worked a new mussel', DATE '17-11-24', DATE '2024-12-17');
 
 //Connect a user with excerises they completed
 INSERT INTO Workout VALUES
-('mscott', 120, 'Row');
+('kking', 120, 'Row');
 
 INSERT INTO Workout VALUES
-('dschrute', 200, 'Pull Down');
+('llewis', 200, 'Pull Down');
 
 INSERT INTO Workout VALUES
-('jhalpert', 250, 'Squats');
+('mmiller', 250, 'Sqauts');
 
 INSERT INTO Workout VALUES
-('shudson', 100, 'Bench Press');
+('nnelson', 100, 'Bicep Curels');
 
 INSERT INTO Workout VALUES
-('phalpert', 0, 'Situps');
+('oowens', 0, 'Situps');
 
 INSERT INTO Workout VALUES
-('abernard', 50, 'Tricep Pulldown');
+('pparker', 50, 'Calf Extension');
+
+INSERT INTO Workout VALUES
+('qquinn', 100, 'Bench Press');
+
+INSERT INTO Workout VALUES
+('rroberts', 0, 'Situps');
+
+INSERT INTO Workout VALUES
+('ssmith', 0, 'Walking');
+
+INSERT INTO Workout VALUES
+('ttaylor', 50, 'Tricep Pulldown');
 
 //conected users with a goal they reached
 INSERT INTO Reach VALUES
-(DATE '2024-11-26', 'mscott', 3);
+(DATE '2024-11-26', 'kking', 3);
 
 INSERT INTO Reach VALUES
-(DATE '2024-11-26', 'dschrute', 1);
+(DATE '2024-11-26', 'llewis', 1);
 
 INSERT INTO Reach VALUES
-(DATE '2024-11-26', 'jhalpert', 6);
+(DATE '2024-11-26', 'mmiller', 6);
 
 INSERT INTO Reach VALUES
-(DATE '2024-11-26', 'shudson', 5);
+(DATE '2024-11-26', 'nnelson', 5);
 
 INSERT INTO Reach VALUES
-(DATE '2024-11-26', 'phalpert', 4);
+(DATE '2024-11-26', 'oowens', 4);
 
 INSERT INTO Reach VALUES
-(DATE '2024-11-26', 'abernard', 2);
+(DATE '2024-11-26', 'pparker', 2);
+
+INSERT INTO Reach VALUES
+(DATE '2024-11-26', 'qquinn', 3);
+
+INSERT INTO Reach VALUES
+(DATE '2024-11-26', 'rroberts', 7);
+
+INSERT INTO Reach VALUES
+(DATE '2024-11-26', 'ssmith', 8);
+
+INSERT INTO Reach VALUES
+(DATE '2024-11-26', 'ttaylor', 8);
 
 //log each users excerise
 INSERT INTO LogTable (Calories , startTime, endTime, username) VALUES
-(500, TIMESTAMP '2024-11-17 1:00:00', TIMESTAMP '2024-11-17 3:00:00', 'mscott');
+(500, TIMESTAMP '2024-11-17 1:00:00', TIMESTAMP '2024-11-17 3:00:00', 'kking');
 
 INSERT INTO LogTable (Calories , startTime, endTime, username) VALUES
-(741, TIMESTAMP '2024-11-17 4:00:00', TIMESTAMP '2024-11-17 4:30:00', 'dschrute');
+(741, TIMESTAMP '2024-11-17 4:00:00', TIMESTAMP '2024-11-17 4:30:00', 'llewis');
 
 INSERT INTO LogTable (Calories , startTime, endTime, username) VALUES
-(650, TIMESTAMP '2024-11-17 7:00:00', TIMESTAMP '2024-11-17 9:00:00', 'jhalpert');
+(650, TIMESTAMP '2024-11-17 7:00:00', TIMESTAMP '2024-11-17 9:00:00', 'mmiller');
 
 INSERT INTO LogTable (Calories , startTime, endTime, username) VALUES
-(400, TIMESTAMP '2024-11-17 3:00:00', TIMESTAMP '2024-11-17 5:00:00', 'shudson');
+(400, TIMESTAMP '2024-11-17 3:00:00', TIMESTAMP '2024-11-17 5:00:00', 'nnelson');
 
 INSERT INTO LogTable (Calories , startTime, endTime, username) VALUES
-(800, TIMESTAMP '2024-11-17 10:00:00', TIMESTAMP '2024-11-17 12:00:00', 'phalpert');
+(800, TIMESTAMP '2024-11-17 10:00:00', TIMESTAMP '2024-11-17 12:00:00', 'oowens');
 
 INSERT INTO LogTable (Calories , startTime, endTime, username) VALUES
-(343, TIMESTAMP '2024-11-17 8:14:00', TIMESTAMP '2024-11-17 10:33:00', 'abernard');
+(343, TIMESTAMP '2024-11-17 8:14:00', TIMESTAMP '2024-11-17 10:33:00', 'pparker');
 
+INSERT INTO LogTable (Calories , startTime, endTime, username) VALUES
+(650, TIMESTAMP '2024-11-17 2:30:00', TIMESTAMP '2024-11-17 3:00:00', 'qquinn');
 
+INSERT INTO LogTable (Calories , startTime, endTime, username) VALUES
+(400, TIMESTAMP '2024-11-17 9:00:00', TIMESTAMP '2024-11-17 10:00:00', 'rroberts');
+
+INSERT INTO LogTable (Calories , startTime, endTime, username) VALUES
+(800, TIMESTAMP '2024-11-17 10:00:00', TIMESTAMP '2024-11-17 11:30:00', 'ssmith');
+
+INSERT INTO LogTable (Calories , startTime, endTime, username) VALUES
+(343, TIMESTAMP '2024-11-17 9:13:00', TIMESTAMP '2024-11-17 10:30:00', 'ttaylor');
